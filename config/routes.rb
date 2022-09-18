6cd7f7a1fcced2416782bcf3940ac9d025b403ed
EC2 Default User <ec2-user@ip-172-31-54-33.ec2.internal>
2022-09-18 12:28:57 +0000

    [Add]game_question

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,17 @@
 Rails.application.routes.draw do
+
+  # 管理者
+  devise_for :admins, skip: [:registrations, :passwords], controllers: {
+    sessions: "admin/session"
+  }
+
+  #ユーザー
+  devise_for :users, skip: [:passwords], controllers: {
+    registrations: "public/registrations",
+    sessions: 'public/sessions'
+  }
+
+
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
