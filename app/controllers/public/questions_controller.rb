25c926101d9b809b0c80c9fcec218d0d6c641a2f
EC2 Default User <ec2-user@ip-172-31-54-33.ec2.internal>
2022-09-26 13:31:18 +0000

    [Add]game_question

--- app/controllers/public/questions_controller.rb
+++ app/controllers/public/questions_controller.rb
@@ -1,9 +1,44 @@
 class Public::QuestionsController < ApplicationController
 
   def index
+    @questions = Question.all
+  end
+
+  def new
+    @question = Question.new
+  end
+
+  def create
+    @question = Question.new(question_params)
+    @question.user_id = current_user.id
+    @question.save
+    # binding.pry
+    redirect_to admin_questions_path
   end
 
   def show
+    @question = Question.find(params[:id])
+  end
+
+  def edit
+    @question = Question.find(params[:id])
+  end
+
+  def update
+    @question = Question.find(params[:id])
+    @question.update(question_params)
+  end
+
+  def destroy
+    @question = Question.find(params[:id])
+    @question.destroy
+    redirect_to admin_questions_path
+  end
+
+  private
+
+  def question_params
+    params.require(:question).permit(:title,:question,:game_id)
   end
 
 end
