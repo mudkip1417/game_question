8e29c712fb959baac6fbdeb6eb4262d838f3dc9f
EC2 Default User <mizupi1417@gmail.com>
2022-11-19 07:18:38 +0000

    [Add]game_question

--- db/migrate/20221002050200_create_tagmaps.rb
+++ db/migrate/20221002050200_create_tagmaps.rb
@@ -1,7 +1,7 @@
 class CreateTagmaps < ActiveRecord::Migration[6.1]
   def change
     create_table :tagmaps do |t|
-      t.references :question, null: false, foreign_key: true
+      t.references :question, type: :bigint, null: false, foreign_key: true
       t.references :tag, null: false, foreign_key: true
 
       t.timestamps
