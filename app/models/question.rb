b05496715426a7ee34d901bc06e0b4b45521c2be
EC2 Default User <mizupi1417@gmail.com>
2022-11-20 09:53:05 +0000

    [Add]game_question

--- app/models/question.rb
+++ app/models/question.rb
@@ -50,7 +50,7 @@ class Question < ApplicationRecord
 
   def get_image
     unless image.attached?
-      file_path = Rails.root.join('app/assets/images/sample-author1.jpg')
+      file_path = Rails.root.join('app/assets/images/no_image.jpg')
       image.attach(io: File.open(file_path), filename: 'default-image.jpg', content_type: 'image/jpeg')
     end
     image
